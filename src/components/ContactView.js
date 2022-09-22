f6c775b36690101bacba4edf39903c83498855ec
Your Name <william.gruszka@gmail.com>
2022-09-22 12:56:53 +1000

    updated all 'p' Typography to use body1 variant

--- src/components/ContactView.js
+++ src/components/ContactView.js
@@ -1,13 +1,10 @@
 import { useState, useEffect } from 'react';
 import { Grid, Paper, Typography, Button, Divider } from '@mui/material';
 
-// import Button from '@mui/material/Button';
-// import Divider from '@mui/material/Divider';
-
 function ContactView(props) {
     const [display, setDisplay] = useState(props.activeContact ? 'block' : 'none');
 
-    //
+    // update disaply value whenever a contact has been selected/ is active
     useEffect(() => {
         setDisplay(props.activeContact ? 'block' : 'none')
     }, [props.activeContact]);
@@ -15,7 +12,7 @@ function ContactView(props) {
     return (
         <Grid item xs={12} md={9} sx={{display: { 'xs':display, 'md':'block' }}}>
             {props.activeContact ? (
-                <Paper elevation={3} sx={{minHeight: "100%", textAlign: "left", padding: "10px"}}>
+                <Paper elevation={1} sx={{minHeight: "100%", textAlign: "left", padding: "10px"}}>
                     <Button 
                         variant="text" 
                         sx={{display: { 'xs':'block', 'md':'none' }}} 
@@ -26,14 +23,14 @@ function ContactView(props) {
                     
                     <Grid className="contact-view-item" container flexDirection="column" justifyContent="space-between" wrap='wrap'>
                         <Typography variant='h3'>{props.activeContact.name}</Typography>
-                        <Typography variant='p'>{props.activeContact.company.name}</Typography>
+                        <Typography variant='body1'>{props.activeContact.company.name}</Typography>
                     </Grid>
 
                     <Divider variant="middle" sx={{margin:'0'}}/>
 
                     <Grid className="contact-view-item" container justifyContent="space-between" wrap='wrap'>
                         <Grid item md={6} xs={12}>
-                        <Typography variant='p'>Mobile Number</Typography>
+                        <Typography variant='body1'>Mobile Number</Typography>
                         </Grid>
                         <Grid item md={6} xs={12} alignItems="center">
                             <a href = {`tel:${props.activeContact.phone}`}>{props.activeContact.phone}</a> 
@@ -44,7 +41,7 @@ function ContactView(props) {
 
                     <Grid className="contact-view-item" container justifyContent="space-between" wrap='wrap'>
                         <Grid item md={6} xs={12}>
-                            <Typography variant='p'>Email</Typography>
+                            <Typography variant='body1'>Email</Typography>
                         </Grid>
                         <Grid item md={6} xs={12}>
                             <a href = {`mailto: ${props.activeContact.email}`}>{props.activeContact.email}</a> 
@@ -55,14 +52,14 @@ function ContactView(props) {
 
                     <Grid className="contact-view-item" container justifyContent="space-between" wrap='wrap'>
                         <Grid item md={6} xs={12}>
-                            <Typography variant='p'>Address</Typography>
+                            <Typography variant='body1'>Address</Typography>
                         </Grid>
                         <Grid item md={4} xs={12}>
-                            <Paper sx={{backgroundColor: 'primary.light', padding: '10px 10px'}}>
-                                <Typography sx={{margin: '12px 0'}} variant='p'>{props.activeContact.address.street}</Typography><br></br>
-                                <Typography sx={{margin: '12px 0'}} variant='p'>{props.activeContact.address.suite}</Typography><br></br>
-                                <Typography sx={{margin: '12px 0'}} variant='p'>{props.activeContact.address.city}</Typography><br></br>
-                                <Typography sx={{margin: '12px 0'}} variant='p'>{props.activeContact.address.zipcode}</Typography><br></br>
+                            <Paper elevation={0} sx={{backgroundColor: 'primary.light', padding: '10px 10px'}}>
+                                <Typography variant='body1'>{props.activeContact.address.street}</Typography>
+                                <Typography variant='body1'>{props.activeContact.address.suite}</Typography>
+                                <Typography variant='body1'>{props.activeContact.address.city}</Typography>
+                                <Typography variant='body1'>{props.activeContact.address.zipcode}</Typography>
                             </Paper>
                         </Grid>
                         <Grid item md={2} xs={0}>
@@ -74,7 +71,7 @@ function ContactView(props) {
 
                     <Grid className="contact-view-item" container justifyContent="space-between" wrap='wrap'>
                         <Grid item md={6} xs={12}>
-                            <Typography variant='p'>Website</Typography>
+                            <Typography variant='body1'>Website</Typography>
                         </Grid>
                         <Grid item md={6} xs={12}>
                             <a href = {props.activeContact.website}>{props.activeContact.website}</a>  
